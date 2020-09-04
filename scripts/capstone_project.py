c820e9663b160d4034b2adf8bf73279d6ceea8c4
avantik-ME <61331298+avantik-ME@users.noreply.github.com>
2020-09-04 18:47:28 -0500

    added a statement to confirm the changes in the capstone python script

--- scripts/capstone_project.py
+++ scripts/capstone_project.py
@@ -1,5 +1,6 @@
 #this is the first weeks submission for Capstone project in the Coursera
 #course title: applied data science capstone,
+#this file will be used in the capstone project henceforth
 #for this project we import pandas and numpy and then print a hello statement
 
 import numpy as np
